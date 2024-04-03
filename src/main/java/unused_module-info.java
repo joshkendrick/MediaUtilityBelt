8b9f59cda88fb5540431e43dd421b51adf230f66
joshkendrick <joshkendrick@users.noreply.github.com>
2024-04-03 08:21:08 -0500

    remove unused files

--- src/main/java/unused_module-info.java
+++
@@ -1,9 +0,0 @@
-/*
-module us.joshkendrick.MediaUtilityBelt {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens us.joshkendrick.MediaUtilityBelt to javafx.fxml;
-    exports us.joshkendrick.MediaUtilityBelt;
-}
-*/
